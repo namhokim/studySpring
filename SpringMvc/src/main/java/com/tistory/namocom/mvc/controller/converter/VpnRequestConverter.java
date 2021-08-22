d52e304c2b21b5c26c4cbae9c850c7176c32b8e1
Namho Kim <nhkimnamo@gmail.com>
2021-08-22 15:37:09 +0900

    springmvc(test): Add JSR-305, 330
    - 305: replace @Required to @Nonnull
    - 330: replace @Component, @Service to @Named or @ManagedBean (이미 포함)

--- SpringMvc/src/main/java/com/tistory/namocom/mvc/controller/converter/VpnRequestConverter.java
+++ SpringMvc/src/main/java/com/tistory/namocom/mvc/controller/converter/VpnRequestConverter.java
@@ -2,12 +2,15 @@ package com.tistory.namocom.mvc.controller.converter;
 
 import com.tistory.namocom.mvc.controller.parameter.VpnRequest;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.stereotype.Component;
 
-@Component
+import javax.annotation.Nonnull;
+import javax.inject.Named;
+
+@Named
 public class VpnRequestConverter implements Converter<String, VpnRequest> {
+
     @Override
-    public VpnRequest convert(String source) {
+    public VpnRequest convert(@Nonnull String source) {
         return new VpnRequest("namo", source);
     }
 }
