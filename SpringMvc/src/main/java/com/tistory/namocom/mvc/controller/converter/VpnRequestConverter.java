6de83f7a150c54abe08dfab5d7ecaf156003aecb
Namho Kim <nhkimnamo@gmail.com>
2021-08-22 15:56:05 +0900

    springmvc(test): Test graceful shutdown

--- SpringMvc/src/main/java/com/tistory/namocom/mvc/controller/converter/VpnRequestConverter.java
+++ SpringMvc/src/main/java/com/tistory/namocom/mvc/controller/converter/VpnRequestConverter.java
@@ -1,16 +1,27 @@
 package com.tistory.namocom.mvc.controller.converter;
 
 import com.tistory.namocom.mvc.controller.parameter.VpnRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.converter.Converter;
 
 import javax.annotation.Nonnull;
 import javax.inject.Named;
+import java.util.UUID;
 
 @Named
+@Slf4j
 public class VpnRequestConverter implements Converter<String, VpnRequest> {
 
     @Override
     public VpnRequest convert(@Nonnull String source) {
+        final UUID uuid = UUID.randomUUID();
+        try {
+            log.info("[{}] Convert {} to VpnRequest", uuid, source);
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("[{}] Convert completed {} to VpnRequest", uuid, source);
         return new VpnRequest("namo", source);
     }
 }
