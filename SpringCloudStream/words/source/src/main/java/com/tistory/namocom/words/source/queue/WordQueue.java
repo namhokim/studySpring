d203bc1fd54aa2a5de38600be317f47158d5250d
Namho Kim <nhkimnamo@gmail.com>
2021-10-11 22:52:13 +0900

    refactor(drinking): reference by interface
    - ConcurrentLinkedQueue to Queue

--- SpringCloudStream/words/source/src/main/java/com/tistory/namocom/words/source/queue/WordQueue.java
+++ SpringCloudStream/words/source/src/main/java/com/tistory/namocom/words/source/queue/WordQueue.java
@@ -3,12 +3,13 @@ package com.tistory.namocom.words.source.queue;
 import com.tistory.namocom.words.model.Word;
 import org.springframework.stereotype.Service;
 
+import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Service
 public class WordQueue {
 
-    private final ConcurrentLinkedQueue<Word> queue = new ConcurrentLinkedQueue<>();
+    private final Queue<Word> queue = new ConcurrentLinkedQueue<>();
 
     public void push(Word word) {
         this.queue.add(word);
