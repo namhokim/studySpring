f433b505f8dd232e7139a1ee6c0df6965a8bac96
namo <nhkimnamo@gmail.com>
2024-05-05 17:18:09 +0900

    feat(project-setting): start study JobRunr init
    - Dependencies: Spring Web
    - https://start.spring.io/#!type=gradle-project-kotlin&language=kotlin&platformVersion=3.2.5&packaging=jar&jvmVersion=21&groupId=com.example&artifactId=study-jobrunr&name=study-jobrunr&description=Demo%20project%20for%20JobRunr&packageName=com.example.study-jobrunr&dependencies=web

---
+++ study-jobrunr/src/main/kotlin/com/example/studyjobrunr/StudyJobrunrApplication.kt
@@ -0,0 +1,11 @@
+package com.example.studyjobrunr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyJobrunrApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudyJobrunrApplication>(*args)
+}
