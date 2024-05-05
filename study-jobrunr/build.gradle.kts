94d1aa0c86ce03597c308a8e7c1b85e362932984
namo <nhkimnamo@gmail.com>
2024-05-05 18:18:17 +0900

    feat(project-setting): add dependency of JobRunr
    - Error:
    ```java
    Parameter 0 of method jobScheduler in org.jobrunr.spring.autoconfigure.JobRunrAutoConfiguration required a bean of type 'org.jobrunr.storage.StorageProvider' that could not be found.
    ```

--- study-jobrunr/build.gradle.kts
+++ study-jobrunr/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jobrunr:jobrunr-spring-boot-3-starter:7.1.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
