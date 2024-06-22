77b0f29bd822d1c6edce0420f0fea8fd45fac55a
namo <nhkimnamo@gmail.com>
2024-06-22 21:59:35 +0900

    docs(spring-modulith): module dependencies info
    - Generate module dependencies document
    - Actuator

--- SpringModulith/src/test/kotlin/com/example/service/ServiceApplicationTests.kt
+++ SpringModulith/src/test/kotlin/com/example/service/ServiceApplicationTests.kt
@@ -3,6 +3,7 @@ package com.example.service
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.modulith.core.ApplicationModules
+import org.springframework.modulith.docs.Documenter
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("test")
@@ -12,7 +13,12 @@ class ServiceApplicationTests {
 	@Test
 	fun contextLoads() {
 		val modules: ApplicationModules = ApplicationModules.of(ServiceApplication::class.java)
+		modules.forEach { println("module: ${it.name} : ${it.basePackage}") }
 		modules.verify()
+
+		Documenter(modules)
+			.writeIndividualModulesAsPlantUml()
+			.writeModulesAsPlantUml()
 	}
 
 }
