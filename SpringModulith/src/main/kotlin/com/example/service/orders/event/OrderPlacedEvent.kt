6fe04a13dc4ee180b96e4a461681aeee6561d341
namo <nhkimnamo@gmail.com>
2024-08-10 22:06:56 +0900

    chore(spring-retry): Sprint Retry with Java - init

--- SpringModulith/src/main/kotlin/com/example/service/orders/event/OrderPlacedEvent.kt
+++ SpringModulith/src/main/kotlin/com/example/service/orders/event/OrderPlacedEvent.kt
@@ -1,8 +1,11 @@
-package com.example.service.orders
+package com.example.service.orders.event
 
 import com.example.service.orders.configuration.OrdersIntegrationConfiguration
+import com.example.service.products.ProductService
+import org.springframework.modulith.ApplicationModule
 import org.springframework.modulith.events.Externalized
 
+@ApplicationModule(allowedDependencies = ["products"])
 @Externalized(target = OrdersIntegrationConfiguration.ORDERS_DESTINATION)
 data class OrderPlacedEvent(
     val orderId: Int,
---
+++ retryjava/src/test/java/com/example/retryjava/RetryJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retryjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetryJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
