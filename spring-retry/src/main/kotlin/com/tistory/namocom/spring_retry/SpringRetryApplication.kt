224b94b013a732966bfaa7b6c786bc0174c37e15
namo <nhkimnamo@gmail.com>
2024-10-27 13:13:39 +0900

    feat(spring-retry): initialize demo project file

---
+++ spring-retry/src/main/kotlin/com/tistory/namocom/spring_retry/SpringRetryApplication.kt
@@ -0,0 +1,11 @@
+package com.tistory.namocom.spring_retry
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRetryApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringRetryApplication>(*args)
+}
