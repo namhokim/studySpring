ca15279f0e78b31e690f003aea0c7cd284b6a87f
namo <nhkimnamo@gmail.com>
2025-01-22 14:53:10 +0900

    Init with Java Persistence with Spring Data and Hibernate
    - share:  https://start.spring.io/#!type=gradle-project-kotlin&language=kotlin&platformVersion=3.4.1&packaging=jar&jvmVersion=21&groupId=com.manning.javapersistence&artifactId=springdatajpa&name=springdatajpa&description=Spring%20Data%20with%20Spring%20Boot&packageName=com.manning.javapersistence.springdatajpa&dependencies=data-jpa,mysql
    
    - English: https://www.manning.com/books/java-persistence-with-spring-data-and-hibernate
    - Korean: https://www.yes24.com/Product/Goods/124899724

---
+++ springdatajpa/springdatajpa/src/main/kotlin/com/manning/javapersistence/springdatajpa/SpringdatajpaApplication.kt
@@ -0,0 +1,29 @@
+package com.manning.javapersistence.springdatajpa
+
+import com.manning.javapersistence.springdatajpa.model.User
+import com.manning.javapersistence.springdatajpa.repositories.UserRepository
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.time.LocalDate
+import java.time.Month
+
+@SpringBootApplication
+@Suppress("SpellCheckingInspection")
+class SpringdatajpaApplication {
+    @Bean
+    fun configure(userRepository: UserRepository) = ApplicationRunner {
+        val user1 = User(username = "beth", registrationDate = LocalDate.of(2020, Month.AUGUST, 3))
+        val user2 = User(username = "mike", registrationDate = LocalDate.of(2020, Month.JANUARY, 18))
+
+        userRepository.save(user1)
+        userRepository.save(user2)
+
+        userRepository.findAll().forEach { println(it) }
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<SpringdatajpaApplication>(*args)
+}
---
+++ springdatajpa/springdatajpa/src/main/kotlin/com/manning/javapersistence/springdatajpa/model/User.kt
@@ -0,0 +1,17 @@
+package com.manning.javapersistence.springdatajpa.model
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDate
+
+@Entity
+@Table
+data class User(
+    @Id
+    @GeneratedValue
+    val id: Long? = null,
+    val username: String,
+    val registrationDate: LocalDate,
+)
