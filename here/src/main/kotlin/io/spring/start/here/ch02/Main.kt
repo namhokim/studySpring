1962f81d3df1622e034359e77b8ed57c14fbbc59
namo <nhkimnamo@gmail.com>
2026-03-14 23:36:30 +0900

    feat(spring-start-here): Ch2. 스프링 컨텍스트 인스턴스 생성하기
    - Parrot 인스턴스를 생성했지만 스프링 컨텍스트 외부에 있다.

--- here/src/main/kotlin/io/spring/start/here/ch02/Main.kt
+++ here/src/main/kotlin/io/spring/start/here/ch02/Main.kt
@@ -1,6 +1,9 @@
 package io.spring.start.here.ch02
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+
 fun main() {
+    AnnotationConfigApplicationContext()
     val parrot = Parrot("Polly")
     println(parrot)
 }
