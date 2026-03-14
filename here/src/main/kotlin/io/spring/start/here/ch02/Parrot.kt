2a61b1bbcc3c26fa1e97fa62183a03cde92902dd
namo <nhkimnamo@gmail.com>
2026-03-15 00:10:06 +0900

    feat(spring-start-here): Ch2. 스프링 컨텍스트에 빈 추가3
    - 프로그래밍 방식으로 스프링 컨텍스트에 빈 추가

--- here/src/main/kotlin/io/spring/start/here/ch02/Parrot.kt
+++ here/src/main/kotlin/io/spring/start/here/ch02/Parrot.kt
@@ -1,14 +1,5 @@
 package io.spring.start.here.ch02
 
-import jakarta.annotation.PostConstruct
-import org.springframework.stereotype.Component
-
-@Component
 data class Parrot(
     var name: String? = null,
-) {
-    @PostConstruct
-    fun init() {
-        this.name = "Parrot"
-    }
-}
+)
