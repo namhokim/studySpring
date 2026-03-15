8eb2cbf327069966fc5484ef5cc369e0d055a503
namo <nhkimnamo@gmail.com>
2026-03-16 00:04:13 +0900

    feat(spring-start-here): Ch3. 순환 의존성 다루기
    - 교착 상태: Parrot <-> Person
    ```
    Error creating bean with name 'parrot': Requested bean is currently in creation: Is there an unresolvable circular reference or an asynchronous initialization dependency?
    ```

--- here/src/main/kotlin/io/spring/start/here/ch03/Parrot.kt
+++ here/src/main/kotlin/io/spring/start/here/ch03/Parrot.kt
@@ -1,11 +1,14 @@
 package io.spring.start.here.ch03
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-data class Parrot(
-    val name: String = "Koko",
+data class Parrot @Autowired constructor(
+    val person: Person,
 ) {
+    val name: String = "Koko"
+
     override fun toString(): String {
         return "Parrot: $name"
     }
