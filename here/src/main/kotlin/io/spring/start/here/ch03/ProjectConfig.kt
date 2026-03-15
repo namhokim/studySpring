63f2a29507d5ab6347d97875fcc6484feea30112
namo <nhkimnamo@gmail.com>
2026-03-16 00:17:57 +0900

    feat(spring-start-here): Ch3. 스프링 컨텍스트에서 여러 빈 중 선택하기2
    - 매개변수의 식별자에 의존하는 대신 생성자에서 `@qualifier` 애너테이션을 사용하여 특정 빈을 명시적으로 선택하기
    ```
    No qualifying bean of type 'io.spring.start.here.ch03.Parrot' available: expected single matching bean but found 2: parrot1,parrot2
    ```

--- here/src/main/kotlin/io/spring/start/here/ch03/ProjectConfig.kt
+++ here/src/main/kotlin/io/spring/start/here/ch03/ProjectConfig.kt
@@ -1,10 +1,11 @@
 package io.spring.start.here.ch03
 
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@ComponentScan(basePackages = ["io.spring.start.here.ch03"])
 class ProjectConfig {
     @Bean
     fun parrot1(): Parrot {
@@ -14,10 +15,5 @@ class ProjectConfig {
     @Bean
     fun parrot2(): Parrot {
         return Parrot("Miki")
-    }
-
-    @Bean
-    fun person(@Qualifier("parrot2") parrot: Parrot): Person {
-        return Person(parrot)
     }
 }
