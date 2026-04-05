0a19e3a5fd0e7eec43bbe6004f7386bb2ab4c470
namo <nhkimnamo@gmail.com>
2026-04-05 21:18:17 +0900

    feat: Initialize Spring Boot project structure for boot3 and boot4
    - boot 3: https://start.spring.io/#!type=gradle-project-kotlin&language=kotlin&platformVersion=3.5.13&packaging=jar&configurationFileFormat=properties&jvmVersion=21&groupId=com.tistory.findnamo&artifactId=boot4&packageName=com.tistory.findnamo.boot4&dependencies=cache,web,data-redis
    - boot 4: https://start.spring.io/#!type=gradle-project-kotlin&language=kotlin&platformVersion=4.0.5&packaging=jar&configurationFileFormat=properties&jvmVersion=21&groupId=com.tistory.findnamo&artifactId=boot4&packageName=com.tistory.findnamo.boot4&dependencies=cache,web,data-redis

---
+++ redis/boot3/src/main/kotlin/com/tistory/findnamo/boot3/Boot3Application.kt
@@ -0,0 +1,11 @@
+package com.tistory.findnamo.boot3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Boot3Application
+
+fun main(args: Array<String>) {
+	runApplication<Boot3Application>(*args)
+}
