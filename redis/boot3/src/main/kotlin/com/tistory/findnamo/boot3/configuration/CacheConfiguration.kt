aa54496ca361e3526b351933f71db3c63d78d417
namo <nhkimnamo@gmail.com>
2026-04-05 23:50:14 +0900

    feat(boot3): JdkSerializationRedisSerializer 전체 적용

--- redis/boot3/src/main/kotlin/com/tistory/findnamo/boot3/configuration/CacheConfiguration.kt
+++ redis/boot3/src/main/kotlin/com/tistory/findnamo/boot3/configuration/CacheConfiguration.kt
@@ -2,33 +2,40 @@ package com.tistory.findnamo.boot3.configuration
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.tistory.findnamo.boot3.controller.TestEntity
-import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer
+import org.springframework.boot.autoconfigure.cache.CacheProperties
+import org.springframework.boot.autoconfigure.cache.CacheProperties.Redis
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
-import java.time.Duration
 
 @Configuration(proxyBeanMethods = false)
 @EnableCaching
 class CacheConfiguration(
+    private val cacheProperties: CacheProperties,
     private val objectMapper: ObjectMapper,
 ) {
     @Bean
-    fun redisCacheManagerBuilderCustomizer(): RedisCacheManagerBuilderCustomizer {
-        return RedisCacheManagerBuilderCustomizer { builder ->
-            builder
-                .withCacheConfiguration(
-                    "test", RedisCacheConfiguration
-                        .defaultCacheConfig().entryTtl(Duration.ofMinutes(1))
-                        .serializeValuesWith(
-                            RedisSerializationContext.SerializationPair.fromSerializer(
-                                Jackson2JsonRedisSerializer(objectMapper, TestEntity::class.java)
-                            )
-                        )
+    fun redisCacheConfiguration(): RedisCacheConfiguration {
+        val redisProperties: Redis = cacheProperties.redis
+        val config: RedisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    Jackson2JsonRedisSerializer(objectMapper, TestEntity::class.java)
                 )
-        }
+            )
+            .applyIfNotNull(redisProperties.timeToLive) { entryTtl(redisProperties.timeToLive) }
+            .applyIfNotNull(redisProperties.keyPrefix) { prefixCacheNameWith(redisProperties.keyPrefix) }
+            .applyIfNot(redisProperties.isCacheNullValues) { disableCachingNullValues() }
+            .applyIfNot(redisProperties.isUseKeyPrefix) { disableKeyPrefix() }
+        return config
     }
+
+    private inline fun <T, R> T.applyIfNotNull(value: R?, block: T.(R) -> T): T =
+        if (value != null) block(value) else this
+
+    private inline fun <T> T.applyIfNot(condition: Boolean, block: T.() -> T): T =
+        if (!condition) block() else this
 }
