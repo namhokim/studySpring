c1e642437c1ab0d0703552da84f9c0eae2aea529
namo <nhkimnamo@gmail.com>
2026-04-05 23:05:18 +0900

    feat(boot3): JdkSerializationRedisSerializer

--- redis/boot3/src/main/kotlin/com/tistory/findnamo/boot3/configuration/CacheConfiguration.kt
+++ redis/boot3/src/main/kotlin/com/tistory/findnamo/boot3/configuration/CacheConfiguration.kt
@@ -1,10 +1,34 @@
 package com.tistory.findnamo.boot3.configuration
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.tistory.findnamo.boot3.controller.TestEntity
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import java.time.Duration
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 @EnableCaching
-class CacheConfiguration{
-
+class CacheConfiguration(
+    private val objectMapper: ObjectMapper,
+) {
+    @Bean
+    fun redisCacheManagerBuilderCustomizer(): RedisCacheManagerBuilderCustomizer {
+        return RedisCacheManagerBuilderCustomizer { builder ->
+            builder
+                .withCacheConfiguration(
+                    "test", RedisCacheConfiguration
+                        .defaultCacheConfig().entryTtl(Duration.ofMinutes(1))
+                        .serializeValuesWith(
+                            RedisSerializationContext.SerializationPair.fromSerializer(
+                                Jackson2JsonRedisSerializer(objectMapper, TestEntity::class.java)
+                            )
+                        )
+                )
+        }
+    }
 }
--- redis/boot3/src/main/kotlin/com/tistory/findnamo/boot3/controller/TestEntity.kt
+++ redis/boot3/src/main/kotlin/com/tistory/findnamo/boot3/controller/TestEntity.kt
@@ -1,13 +1,6 @@
 package com.tistory.findnamo.boot3.controller
 
-import java.io.Serializable
-
 data class TestEntity(
     val id: Long,
     val name: String,
-): Serializable {
-    companion object {
-        @JvmStatic
-        private val serialVersionUID = 1L
-    }
-}
+)
